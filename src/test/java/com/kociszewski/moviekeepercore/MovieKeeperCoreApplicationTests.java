8c293c6ebec6d49532af4faf9c40daad16753e3c
Xaroun <mateusz1003@gmail.com>
2020-01-13 20:49:36 +0100

    initial commit

---
+++ src/test/java/com/kociszewski/moviekeepercore/MovieKeeperCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kociszewski.moviekeepercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieKeeperCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
