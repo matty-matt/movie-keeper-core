b44d74282824005ad22df2b67ee6b6e572b3d084
Xaroun <mateusz1003@gmail.com>
2020-01-13 23:08:32 +0100

    env var

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # movie-keeper-core
+
+To run application you have to provide TMDb ApiKey in environment variable named TMDB_API_KEY.
---
+++ src/main/java/com/kociszewski/moviekeepercore/proxy/Tmdb.java
@@ -0,0 +1,12 @@
+package com.kociszewski.moviekeepercore.proxy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Tmdb {
+
+    public Tmdb(@Value("${api.key}") String apiKey) {
+        System.out.println(apiKey);
+    }
+}
