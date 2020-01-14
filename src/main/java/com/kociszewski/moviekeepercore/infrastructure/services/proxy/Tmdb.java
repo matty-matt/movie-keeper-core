9bd3af58a4035aea46d126b5d7ef083b25fe9b22
Xaroun <mateusz1003@gmail.com>
2020-01-14 20:32:03 +0100

    structure

--- src/main/java/com/kociszewski/moviekeepercore/infrastructure/services/proxy/Tmdb.java
+++ src/main/java/com/kociszewski/moviekeepercore/infrastructure/services/proxy/Tmdb.java
@@ -1,4 +1,4 @@
-package com.kociszewski.moviekeepercore.proxy;
+package com.kociszewski.moviekeepercore.infrastructure.services.proxy;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
