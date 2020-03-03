081a6e5cd607dbb17f50903fc66a71f330214b6c
Xaroun <mateusz1003@gmail.com>
2020-03-03 23:14:10 +0100

    tests

--- src/test/java/com/kociszewski/moviekeepercore/domain/movie/MovieAggregateTest.java
+++ src/test/java/com/kociszewski/moviekeepercore/domain/movie/MovieAggregateTest.java
@@ -10,6 +10,8 @@ import com.kociszewski.moviekeepercore.domain.movie.events.MovieDeletedEvent;
 import com.kociszewski.moviekeepercore.domain.movie.events.MovieSavedEvent;
 import com.kociszewski.moviekeepercore.domain.movie.events.MovieSearchDelegatedEvent;
 import com.kociszewski.moviekeepercore.domain.movie.events.ToggleWatchedEvent;
+import com.kociszewski.moviekeepercore.domain.movie.info.*;
+import com.kociszewski.moviekeepercore.domain.movie.info.Runtime;
 import com.kociszewski.moviekeepercore.domain.trailer.events.TrailersDeletedEvent;
 import com.kociszewski.moviekeepercore.shared.model.*;
 import org.axonframework.test.aggregate.AggregateTestFixture;
@@ -20,6 +22,8 @@ import org.junit.jupiter.api.Test;
 import java.util.Collections;
 import java.util.Date;
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MovieAggregateTest {
     private static final String TITLE = "some title";
@@ -61,7 +65,13 @@ public class MovieAggregateTest {
     public void shouldEmitMovieSearchDelegatedEvent() {
         fixture.givenNoPriorActivity()
                 .when(new FindMovieCommand(movieId, trailerEntityId, castEntityId, searchPhrase))
-                .expectEvents(new MovieSearchDelegatedEvent(movieId, trailerEntityId, castEntityId, searchPhrase));
+                .expectEvents(new MovieSearchDelegatedEvent(movieId, trailerEntityId, castEntityId, searchPhrase))
+                .expectState(state -> {
+                    assertThat(state.getMovieId()).isEqualTo(movieId);
+                    assertThat(state.getTrailerEntity().getTrailerEntityId()).isEqualTo(trailerEntityId);
+                    assertThat(state.getCastEntity().getCastEntityId()).isEqualTo(castEntityId);
+                    assertThat(state.getSearchPhrase()).isEqualTo(searchPhrase);
+                });
     }
 
     @Test
@@ -69,7 +79,24 @@ public class MovieAggregateTest {
         fixture.given(
                     new MovieSearchDelegatedEvent(movieId, trailerEntityId, castEntityId, searchPhrase))
                 .when(new SaveMovieCommand(movieId, externalMovie))
-                .expectEvents(new MovieSavedEvent(movieId, externalMovie));
+                .expectEvents(new MovieSavedEvent(movieId, externalMovie))
+                .expectState(state -> {
+                    assertThat(state.getExternalMovieId()).isEqualTo(externalMovie.getExternalMovieId());
+                    assertThat(state.getPoster()).isEqualTo(new Poster(externalMovie.getExternalMovieInfo().getPosterPath()));
+                    assertThat(state.getTitle()).isEqualTo(new Title(externalMovie.getExternalMovieInfo().getTitle()));
+                    assertThat(state.getOriginalTitle()).isEqualTo(new Title(externalMovie.getExternalMovieInfo().getOriginalTitle()));
+                    assertThat(state.getOverview()).isEqualTo(new Overview(externalMovie.getExternalMovieInfo().getOverview()));
+                    assertThat(state.getDigitalRelease()).isEqualTo(new Release(externalMovie.getDigitalRelease()));
+                    assertThat(state.getPremiereRelease()).isEqualTo(new Release(externalMovie.getExternalMovieInfo().getReleaseDate()));
+                    assertThat(state.getVote()).isEqualTo(
+                            new Vote(externalMovie.getExternalMovieInfo().getVoteAverage(), externalMovie.getExternalMovieInfo().getVoteCount()));
+                    assertThat(state.getRuntime()).isEqualTo(new Runtime(externalMovie.getExternalMovieInfo().getRuntime()));
+                    assertThat(state.getInsertionDate()).isEqualTo(new DateWrapper(externalMovie.getExternalMovieInfo().getInsertionDate()));
+                    assertThat(state.getLastRefreshDate()).isEqualTo(new DateWrapper(externalMovie.getExternalMovieInfo().getLastRefreshDate()));
+                    assertThat(state.getWatched()).isEqualTo(new Watched(false));
+                    assertThat(state.getOriginalLanguage()).isEqualTo(new Language(externalMovie.getExternalMovieInfo().getOriginalLanguage()));
+                    assertThat(state.getGenres()).isEqualTo(externalMovie.getExternalMovieInfo().getGenres());
+                });
     }
 
     @Test
@@ -78,7 +105,8 @@ public class MovieAggregateTest {
                     new MovieSearchDelegatedEvent(movieId, trailerEntityId, castEntityId, searchPhrase),
                     new MovieSavedEvent(movieId, externalMovie))
                 .when(new ToggleWatchedCommand(movieId, new Watched(true)))
-                .expectEvents(new ToggleWatchedEvent(movieId, new Watched(true)));
+                .expectEvents(new ToggleWatchedEvent(movieId, new Watched(true)))
+                .expectState(state -> assertThat(state.getWatched().isWatched()).isTrue());
     }
 
     @Test
