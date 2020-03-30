3e8b92ca7495c5e641189601da931abe367b7b15
Xaroun <mateusz1003@gmail.com>
2020-03-30 19:44:58 +0200

    await in few tests

--- src/test/java/com/kociszewski/moviekeepercore/domain/trailer/TrailersIntegrationTest.java
+++ src/test/java/com/kociszewski/moviekeepercore/domain/trailer/TrailersIntegrationTest.java
@@ -3,7 +3,9 @@ package com.kociszewski.moviekeepercore.domain.trailer;
 import com.kociszewski.moviekeepercore.domain.CommonIntegrationSetup;
 import com.kociszewski.moviekeepercore.infrastructure.movie.MovieDTO;
 import com.kociszewski.moviekeepercore.infrastructure.trailer.TrailerDTO;
+import com.kociszewski.moviekeepercore.infrastructure.trailer.TrailerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -12,16 +14,28 @@ import java.util.List;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.awaitility.Durations.FIVE_SECONDS;
+import static org.awaitility.Durations.ONE_HUNDRED_MILLISECONDS;
 
 public class TrailersIntegrationTest extends CommonIntegrationSetup {
 
     private static final String GET_TRAILERS_URL = "http://localhost:%d/movies/%s/videos";
+
+    @Autowired
+    private TrailerRepository trailerRepository;
 
     @Test
     public void shouldRetrieveTrailers() {
         // given
         ResponseEntity<MovieDTO> storedMovieResponse = storeMovie(SUPER_MOVIE);
         String movieId = Objects.requireNonNull(storedMovieResponse.getBody()).getAggregateId();
+
+        await()
+                .atMost(FIVE_SECONDS)
+                .with()
+                .pollInterval(ONE_HUNDRED_MILLISECONDS)
+                .until(() -> trailerRepository.findByMovieId(movieId).isPresent());
 
         // when
         ResponseEntity<TrailerDTO[]> trailerResponse = testRestTemplate
