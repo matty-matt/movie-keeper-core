489756060b1d59b4db2c4a83c386e9d974f7bb5f
Xaroun <mateusz1003@gmail.com>
2020-05-06 21:14:46 +0200

    cleaning

--- proxy-service/src/main/java/com/kociszewski/moviekeeper/domain/events/MovieFetchDelegatedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.kociszewski.moviekeeper.domain.events;
-
-import lombok.Value;
-
-@Value
-public class MovieFetchDelegatedEvent {
-    private String proxyId;
-    private String searchPhrase;
-}
--- proxy-service/src/main/java/com/kociszewski/moviekeeper/infrastructure/SearchPhrase.java
+++
@@ -1,8 +0,0 @@
-package com.kociszewski.moviekeeper.infrastructure;
-
-import lombok.Value;
-
-@Value
-public class SearchPhrase {
-    private String phrase;
-}
