483cff353b1b68999e2a07f4d32a74f1103d3070
Mateusz <mateusz1003@gmail.com>
2020-06-01 21:02:46 +0200

    better integratin tests (#20)
    Co-authored-by: mkociszewski <mkociszewski@consdata.pl>

---
+++ trailer-service/src/test/java/com/kociszewski/moviekeeper/integration/MockProxyCommandHandler.java
@@ -0,0 +1,27 @@
+package com.kociszewski.moviekeeper.integration;
+
+import com.kociszewski.moviekeeper.domain.commands.FetchTrailersDetailsCommand;
+import com.kociszewski.moviekeeper.domain.events.TrailersDetailsFetchedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventhandling.gateway.EventGateway;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import static com.kociszewski.moviekeeper.integration.CommonIntegrationSetup.TRAILERS;
+
+@Profile("test")
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MockProxyCommandHandler {
+
+    private final EventGateway eventGateway;
+
+    @CommandHandler
+    public void handle(FetchTrailersDetailsCommand command) {
+        log.info("MOCK fetching...");
+        eventGateway.publish(new TrailersDetailsFetchedEvent(command.getProxyId(), TRAILERS));
+    }
+}
