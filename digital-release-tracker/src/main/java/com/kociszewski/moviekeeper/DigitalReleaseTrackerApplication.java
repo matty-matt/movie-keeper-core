aa8df90a38ddb3a0197ecc387561c90cf7265f07
Xaroun <mateusz1003@gmail.com>
2020-06-25 18:47:46 +0200

    initial digital release tracker commit

---
+++ digital-release-tracker/src/main/java/com/kociszewski/moviekeeper/DigitalReleaseTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.kociszewski.moviekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalReleaseTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalReleaseTrackerApplication.class, args);
+    }
+}
