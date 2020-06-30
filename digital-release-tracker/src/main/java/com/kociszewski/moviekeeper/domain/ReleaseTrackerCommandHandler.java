0c64f94203ce80354fe7eac8d1593b112becbd7e
Xaroun <mateusz1003@gmail.com>
2020-06-30 21:10:09 +0200

    fix

--- digital-release-tracker/src/main/java/com/kociszewski/moviekeeper/domain/ReleaseTrackerCommandHandler.java
+++ digital-release-tracker/src/main/java/com/kociszewski/moviekeeper/domain/ReleaseTrackerCommandHandler.java
@@ -4,6 +4,7 @@ package com.kociszewski.moviekeeper.domain;
 import com.kociszewski.moviekeeper.domain.commands.CreateRefreshMoviesCommand;
 import com.kociszewski.moviekeeper.domain.events.RefreshMoviesDelegatedEvent;
 import com.kociszewski.moviekeeper.domain.queries.GetNotSeenMoviesQuery;
+import com.kociszewski.moviekeeper.domain.queries.GetRefreshedMoviesQuery;
 import com.kociszewski.moviekeeper.infrastructure.MovieDTO;
 import com.kociszewski.moviekeeper.infrastructure.RefreshMovie;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +13,10 @@ import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventhandling.gateway.EventGateway;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
+import org.axonframework.queryhandling.QueryUpdateEmitter;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,6 +27,7 @@ public class ReleaseTrackerCommandHandler {
 
     private final EventGateway eventGateway;
     private final QueryGateway queryGateway;
+    private final QueryUpdateEmitter queryUpdateEmitter;
 
     @CommandHandler
     public void handle(CreateRefreshMoviesCommand command) {
@@ -38,6 +42,7 @@ public class ReleaseTrackerCommandHandler {
                 .collect(Collectors.toList());
         if (moviesToRefresh.isEmpty()) {
             log.info("No movies to refresh. Database is empty!");
+            queryUpdateEmitter.emit(GetRefreshedMoviesQuery.class, query -> true, Collections.emptyList());
         } else {
             eventGateway.publish(new RefreshMoviesDelegatedEvent(command.getRefreshId(), moviesToRefresh));
         }
