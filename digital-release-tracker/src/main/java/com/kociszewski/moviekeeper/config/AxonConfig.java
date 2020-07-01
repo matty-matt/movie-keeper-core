5877bdbae862c2931644f4102c998692da18a046
Xaroun <mateusz1003@gmail.com>
2020-07-01 21:15:40 +0200

    ReleaseTrackerSagas are no longer replaying

---
+++ digital-release-tracker/src/main/java/com/kociszewski/moviekeeper/config/AxonConfig.java
@@ -0,0 +1,21 @@
+package com.kociszewski.moviekeeper.config;
+
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.TrackingEventProcessorConfiguration;
+import org.axonframework.messaging.StreamableMessageSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AxonConfig {
+
+    @Autowired
+    public void customConfig(EventProcessingConfigurer configurer) {
+        var trackingProcessorConfig = TrackingEventProcessorConfiguration
+                .forSingleThreadedProcessing()
+                .andInitialTrackingToken(StreamableMessageSource::createHeadToken);
+        configurer.registerTrackingEventProcessor("ReleaseTrackerSagaProcessor",
+                org.axonframework.config.Configuration::eventStore,
+                c -> trackingProcessorConfig);
+    }
+}
