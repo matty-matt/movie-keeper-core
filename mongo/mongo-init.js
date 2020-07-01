b4cb15a64a13fab8ae085698a5ef46402d62fa66
Xaroun <mateusz1003@gmail.com>
2020-07-01 20:18:32 +0200

    index on watched

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,3 +1,4 @@
 db.movies.createIndex({externalMovieId: 1}, {unique: true});
+db.movies.createIndex({watched: 1}, {unique: false});
 db.trailers.createIndex({externalMovieId: 1}, {unique: true});
 db.cast.createIndex({externalMovieId: 1}, {unique: true});
