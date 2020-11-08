26d14d6a4b6c04f07ccbdc33b406d77bf6c44871
mkociszewski <mkociszewski@consdata.pl>
2020-11-08 16:43:31 +0100

    Failing tests

--- digital-release-tracker/src/test/java/com/kociszewski/moviekeeper/integration/common/TestContainersSetup.java
+++ digital-release-tracker/src/test/java/com/kociszewski/moviekeeper/integration/common/TestContainersSetup.java
@@ -31,10 +31,7 @@ public class TestContainersSetup {
         mongo = new GenericContainer("mongo:latest")
                 .withExposedPorts(MONGO_PORT)
                 .withEnv("MONGO_INITDB_DATABASE", "moviekeeper")
-                .withCommand(String.format("mongod --port %d", MONGO_PORT))
-                .waitingFor(
-                        Wait.forLogMessage(".*waiting for connections.*", 1)
-                );
+                .withCommand(String.format("mongod --port %d", MONGO_PORT));
         mongo.start();
 
         System.setProperty("ENV_MONGO_PORT", String.valueOf(mongo.getMappedPort(MONGO_PORT)));
